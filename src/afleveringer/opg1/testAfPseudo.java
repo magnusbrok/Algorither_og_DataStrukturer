8ffc97cdd69b25334efa6fabe2872fe79aa6bcbe
magnusbrok <43874955+magnusbrok@users.noreply.github.com>
2020-02-20 11:16:16 +0100

    tilføjet svar til 1- 1.3 og skrevet Pseudokoden for opgaven

---
+++ src/afleveringer/opg1/testAfPseudo.java
@@ -0,0 +1,35 @@
+package afleveringer.opg1;
+
+public class testAfPseudo {
+
+    public static void main(String[] args) {
+        testAfPseudo test = new testAfPseudo();
+        int[] A = new int[]{3, 2, 2, 2, 4, 5, 5, 3};
+
+        System.out.println(test.ALG1(A, A.length));
+
+
+
+    }
+
+    public int ALG1(int[] A, int n){
+        int l = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (ALG2(A, i, j) && (j - i + 1) > l) {
+                    l = j-i+1;
+                }
+            }
+        }
+        return l;
+    }
+
+    public boolean ALG2(int[] A, int i, int j){
+        if (i == j) return true;
+        int k = i;
+        for (k = i; k < j; k++) {
+            if (A[k] != A[k+1]) return false;
+        }
+        return true;
+    }
+}
