da4348722e7e4cca26def33b7d8352ef14c87ef9
Siff <siff.ravn@gmail.com>
2020-02-22 21:21:02 +0100

    Aflevering1 FunWithArrays: Bug fix ALG1()
    ALG1() pass all tests now

--- src/afleveringer/opg1/FunWithArrays.java
+++ src/afleveringer/opg1/FunWithArrays.java
@@ -10,11 +10,14 @@ public class FunWithArrays {
             int i = 0;
             int j = i+1;
 
-            while (i+l < n){
+            while (i < n-1 && j < n){
 
-                if (A[i] == A[j] && j-i+1 > l){
+                if (A[i] == A[j]){
 
-                    l = j-i+1;
+                    if (j-i+1 > l){
+                        l = j-i+1;
+                    }
+
                     j++;
 
                 }else{
