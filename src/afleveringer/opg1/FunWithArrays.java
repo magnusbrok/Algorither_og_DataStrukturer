77475b0c45a5211995cc9a4db9abeffcb9545b95
Siff <siff.ravn@gmail.com>
2020-02-23 23:04:29 +0100

    Aflevering1 FunWithArrays: Transfer ALG1 and ALG2 to FunWithArrays

--- src/afleveringer/opg1/FunWithArrays.java
+++ src/afleveringer/opg1/FunWithArrays.java
@@ -3,6 +3,28 @@ package afleveringer.opg1;
 public class FunWithArrays {
 
     public int ALG1(int[] A, int n){
+        int l = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (ALG2(A, i, j) && (j - i + 1) > l) {
+                    l = j-i+1;
+                }
+            }
+        }
+        return l;
+    }
+
+    private boolean ALG2(int[] A, int i, int j){
+        if (i == j) return true;
+        int k = i;
+        for (k = i; k < j; k++) {
+            if (A[k] != A[k+1]) return false;
+        }
+        return true;
+    }
+
+
+    public int ALG3(int[] A, int n){
 
         int l = 1;
         int x = 1; // for runtime
--- src/afleveringer/opg1/FunWithArraysTest.java
+++ src/afleveringer/opg1/FunWithArraysTest.java
@@ -6,49 +6,49 @@ import static org.junit.jupiter.api.Assertions.*;
 class FunWithArraysTest {
 
     @Test
-    void ALG1TestRegular1() {
+    void ALG3TestRegular1() {
         int[] A = {3, 2, 2, 2, 4, 5, 5, 3};
         int n = A.length;
 
         FunWithArrays obj = new FunWithArrays();
 
-        int l = obj.ALG1(A,n);
+        int l = obj.ALG3(A,n);
 
         assertEquals(3,l);
     }
 
     @Test
-    void ALG1TestRegular2() {
+    void ALG3TestRegular2() {
         int[] A = {3, 2, 2, 4, 5, 5, 5, 3};
         int n = A.length;
 
         FunWithArrays obj = new FunWithArrays();
 
-        int l = obj.ALG1(A,n);
+        int l = obj.ALG3(A,n);
 
         assertEquals(3,l);
     }
 
     @Test
-    void ALG1TestArrayLengthOne() {
+    void ALG3TestArrayLengthOne() {
         int[] A = {3};
         int n = A.length;
 
         FunWithArrays obj = new FunWithArrays();
 
-        int l = obj.ALG1(A,n);
+        int l = obj.ALG3(A,n);
 
         assertEquals(1,l);
     }
 
     @Test
-    void ALG1TestEmptyArray() {
+    void ALG3TestEmptyArray() {
         int[] A = new int[4];
         int n = A.length;
 
         FunWithArrays obj = new FunWithArrays();
 
-        int l = obj.ALG1(A,n);
+        int l = obj.ALG3(A,n);
 
         assertEquals(4,l);
     }
