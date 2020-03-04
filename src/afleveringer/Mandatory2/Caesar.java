887d77511d6cc8d6761b9938fcdd4bd39f814260
Siff <siff.ravn@gmail.com>
2020-03-04 16:34:26 +0100

    Aflevering 2 (Caesar): Implement simple algorithm handling letters and "delete"
    Test1 passed

--- src/afleveringer/Mandatory2/Caesar.java
+++ src/afleveringer/Mandatory2/Caesar.java
@@ -1,4 +1,35 @@
 package afleveringer.Mandatory2;
 
+import java.util.Stack;
+
 public class Caesar {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static Stack<String> ALG(char[] input, int n){
+
+        int i = 0;
+        Stack<String> outputStack = new Stack<>();
+
+        while (i < n){
+            char curr = input[i];
+
+            if (Character.isLetter(curr)){
+
+                outputStack.push(Character.toString(curr));
+
+            }
+
+            else if (curr == '^' && !outputStack.empty()){
+
+                outputStack.pop();
+
+            }
+            i++;
+
+        }
+        return outputStack;
+    }
 }
