8ed4f3f47462e98b4060f69e160196c13653d935
magnusbrok <magnus.brok@db-data.dk>
2020-03-04 21:00:03 +0100

    Update ReachingPeak.java

--- src/afleveringer/Mandatory2/ReachingPeak.java
+++ src/afleveringer/Mandatory2/ReachingPeak.java
@@ -1,4 +1,36 @@
 package afleveringer.Mandatory2;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ReachingPeak {
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ReachingPeak peakClass = new ReachingPeak();
+
+        int n = scan.nextInt();
+        int [] array = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            array[i] = i;
+        }
+
+        System.out.println(peakClass.findWorstPeak(array, 0, n-1));
+
+    }
+
+    public ArrayList<Integer> findWorstPeak(int[] array, int i, int j){
+
+        ArrayList<Integer> worstSpots = new ArrayList<>();
+
+        if (i == j) {
+            worstSpots.add(0);
+            return worstSpots;
+        } 
+
+
+        return worstSpots;
+    }
 }
