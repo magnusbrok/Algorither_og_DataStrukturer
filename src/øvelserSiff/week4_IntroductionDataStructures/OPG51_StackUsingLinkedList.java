b03b2e1ee581fc6b84e839b84a7fff79bdaa5c9a
Siff <siff.ravn@gmail.com>
2020-03-05 10:13:57 +0100

    Exercises (Week4): Implement main method for code judge submission (Succeeded)

--- src/øvelserSiff/week4_IntroductionDataStructures/OPG51_StackUsingLinkedList.java
+++ src/øvelserSiff/week4_IntroductionDataStructures/OPG51_StackUsingLinkedList.java
@@ -1,13 +1,29 @@
 package øvelserSiff.week4_IntroductionDataStructures;
 
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class OPG51_StackUsingLinkedList {
 
     private static LinkedList<Integer> linkedList =  new LinkedList<>();
 
     public static void main(String[] args) {
+        Scanner inputScan = new Scanner(System.in);
 
+        int n = Integer.parseInt(inputScan.nextLine());
+
+        for (int i = 0; i < n; i++){
+            String str = inputScan.nextLine();
+            Scanner strScan = new Scanner(str);
+
+            if (strScan.next().contains("PU")){
+                Integer integer = Integer.parseInt(strScan.next());
+                push(integer);
+            }
+            else if (!linkedList.isEmpty()){
+                System.out.println(pop());
+            }
+        }
 
 
     }
