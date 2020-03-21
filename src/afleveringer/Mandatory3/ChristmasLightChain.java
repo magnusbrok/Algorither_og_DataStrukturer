969a1e679a95254cee263eba78a65b5cd17bebe7
magnusbrok <magnus.brok@db-data.dk>
2020-03-21 14:01:57 +0100

    christmaslights 9/10 tests complete

--- src/afleveringer/Mandatory3/ChristmasLightChain.java
+++ src/afleveringer/Mandatory3/ChristmasLightChain.java
@@ -1,4 +1,40 @@
 package afleveringer.Mandatory3;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ChristmasLightChain {
+
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        int n;
+        int longestChain = 0;
+
+        n = scan.nextInt();
+        int[] husNumre = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            husNumre[i] = scan.nextInt();
+        }
+        Arrays.sort(husNumre);
+
+
+        int a = 0;
+        int b = 1;
+        while (a < n - 1 && b < n) {
+            if (husNumre[a]+(b-a) == husNumre[b]) {
+                if ((b - a + 1) > longestChain) {
+                    longestChain = b-a+1;
+                }
+                b++;
+            } else {
+                a = b;
+                b = a+1;
+            }
+        }
+        System.out.println(longestChain);
+    }
+
 }
