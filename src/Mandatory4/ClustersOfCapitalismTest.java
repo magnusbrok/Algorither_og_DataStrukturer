9470ac72f103830b718130f8a76cd6601b9838a5
Siff <siff.ravn@gmail.com>
2020-04-04 14:30:43 +0200

    Aflevering 4 (Clusters of capitalism ): Create test

---
+++ src/Mandatory4/ClustersOfCapitalismTest.java
@@ -0,0 +1,44 @@
+package Mandatory4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClustersOfCapitalismTest {
+
+
+    @Test
+    void input1(){
+        int b = 99; // Cost of a bridge
+        int N = 8;  // Number of islands in the republic
+        String surplus =
+                "25\n" +
+                "25\n" +
+                "25\n" +
+                "75\n" +
+                "50\n" +
+                "50\n" +
+                "25\n" +
+                "50";
+        int M = 5;  // Number of existing bridges in the republic
+        String bridges =
+                "4 6\n" +
+                "5 6\n" +
+                "4 5\n" +
+                "0 1\n" +
+                "1 2";
+
+
+//        constructStates(N, M, bridges);
+
+
+//        ALG1(N, M, bridges); // Computes states S and bridges B
+
+//        int S;
+//        int V;
+//        int B;
+//
+//        ALG2()
+    }
+
+}
