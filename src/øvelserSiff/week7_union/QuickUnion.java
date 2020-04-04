0a892b3d9fbb6699ab7c8bb279c557de2d5683c8
Siff <siff.ravn@gmail.com>
2020-04-04 15:50:44 +0200

    w7 (QuickUnion): Implement (passed CodeJudge)

---
+++ src/øvelserSiff/week7_union/QuickUnion.java
@@ -0,0 +1,59 @@
+package øvelserSiff.week7_union;
+
+import java.util.Scanner;
+
+public class QuickUnion {
+
+    private static int[] p;
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+        int M = scan.nextInt();
+
+        scan.nextLine(); // wft? fixes the bug...
+
+        p = init(N);
+
+        for (int k = 0; k < M; k++){
+            String str = scan.nextLine();
+            Scanner strScan = new Scanner(str);
+
+            if (strScan.next().contains("F")){
+                int i = Integer.parseInt(strScan.next());
+                int r = find(i);
+                System.out.println(r);
+            }
+            else {
+                int i = Integer.parseInt(strScan.next());
+                int j = Integer.parseInt(strScan.next());
+                union(i, j);
+            }
+        }
+    }
+
+    public static int[] init(int N){
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++){
+            arr[i] = i;
+        }
+        return arr;
+    }
+
+    public static int find(int i){
+        while (i != p[i]){
+            i = p[i];
+        }
+        return i;
+    }
+
+    public static void union(int i, int j){
+        int ri = find(i);
+        int rj = find(j);
+
+        if(ri != rj){
+            p[ri] = rj;
+        }
+    }
+}
